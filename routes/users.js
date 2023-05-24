c38695f5373fa1299361fdbe93fe6a5b022f43a7
w44t3r <vk.educate@protonmail.ch>
2023-05-24 14:04:11 +0200

    add mocha plugin eslint, fixed eslint problems with user router, add lint run to pipeline

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 /* GET users listing. */
-const users = ["TP", "PS", "DT, VK"];
 router.get('/', (req, res) => {
   res.json('TP, PS, DT, VK');
 });
