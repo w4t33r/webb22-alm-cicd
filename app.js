ca2db20d79117b3c8df631f4e1d2513da9a18e75
w44t3r <vk.educate@protonmail.ch>
2023-05-24 19:26:55 +0200

    rebuild app.js old declaration, rebuild cmd for image dockerfile

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var calc = require("./routes/calc");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const calc = require("./routes/calc");
 
-var indexRouter = require("./routes");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
