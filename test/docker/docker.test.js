e118af2815df4eea62ad44b26c031a564136813b
w44t3r <vk.educate@protonmail.ch>
2023-05-25 04:32:16 +0200

    feat: port changes docker

--- test/docker/docker.test.js
+++ test/docker/docker.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 describe('Background docker runs', function () {
     it('check is it runs', function () {
-        return request('http://localhost:3000')
+        return request('http://localhost:8080')
             .get('/')
             .expect(200);
     });
@@ -9,7 +9,7 @@ describe('Background docker runs', function () {
 
 describe('Background docker runs', function () {
     it('GET /users from docker container', function () {
-        return request('http://localhost:3000')
+        return request('http://localhost:8080')
             .get('/users')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
@@ -19,7 +19,7 @@ describe('Background docker runs', function () {
 
 describe('Background docker runs', function () {
     it('GET /calc from docker container', function () {
-        return request('http://localhost:3000')
+        return request('http://localhost:8080')
             .get('/calc')
             .expect(200);
     });
